61ab985efc812c39f14d5bf8a36b0dc678ac3f2f
Shivani-1998-Devops <shivaniburgahte@gmail.com>
2026-04-15 23:11:26 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  key_name      = var.linux_key
+  key_name      = var.key_name
 
   tags = {
     Name        = "terraform-${var.env}-instance"
